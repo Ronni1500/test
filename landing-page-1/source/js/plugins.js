aea5cf685fbe0cf60084157c00b679fd719ab8e4
rtsarkov <tsarkofff1500@yandex.ru>
2021-01-18 22:14:19 +0300

    :

--- landing-page-1/source/js/plugins.js
+++
@@ -1,3 +0,0 @@
-//=include ../../bower_components/jquery/dist/jquery.js
-//=include ../../bower_components/jquery-colorbox/jquery.colorbox.js
-//=include ../../bower_components/jquery.inputmask/dist/jquery.inputmask.bundle.js
